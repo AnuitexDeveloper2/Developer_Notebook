682fb3e9ec6e890295b7e81b6cc7bc5d33cfa045
Volodymyr Honcharuk <vhoncharuk@amcbridge.com>
2022-02-21 20:43:41 +0200

    check redux dev tools

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,25 +1,33 @@
-import { Store, createStore, applyMiddleware, compose } from 'redux';
+import { Store, createStore, applyMiddleware, compose, AnyAction } from 'redux';
 import thunk from 'redux-thunk';
 import { AppState, rootReducer } from './reducers/rootReducer';
-import { loadState, saveState } from "../helper/localStorage";
+import { loadState, saveState } from '../helper/localStorage';
 
 export function configureStore(): Store<AppState> {
+  const __REDUX_DEVTOOLS_EXTENSION__ = (window as any).window
+    .__REDUX_DEVTOOLS_EXTENSION__
+    ? (window as any).window.__REDUX_DEVTOOLS_EXTENSION__()
+    : null;
+  const persistedState = loadState();
 
-const persistedState = loadState()
+  let store: Store<AppState, AnyAction> = null;
 
-  const store = createStore(
-    rootReducer,
-    persistedState,
-    compose(
-      applyMiddleware(thunk),
-      (window as any).window.__REDUX_DEVTOOLS_EXTENSION__
-      ? (window as any).window.__REDUX_DEVTOOLS_EXTENSION__()
-      : null
-    ),
-  );
+  if (__REDUX_DEVTOOLS_EXTENSION__) {
+    store = createStore(
+      rootReducer,
+      persistedState,
+      compose(applyMiddleware(thunk), __REDUX_DEVTOOLS_EXTENSION__),
+    );
+  } else {
+    store = createStore(
+      rootReducer,
+      persistedState,
+      compose(applyMiddleware(thunk)),
+    );
+  }
 
-  store.subscribe(()=> {
-    saveState(store.getState())
-  })
+  store.subscribe(() => {
+    saveState(store.getState());
+  });
   return store;
 }
